e53ce3e34a20bda702d2f871318b531eb37538e2
curry-bean <curry.bean.017@gmail.com>
2023-02-22 14:05:05 +0300

    5th Commit

--- Text-to-Speech.py
+++ Text-to-Speech.py
@@ -40,8 +40,8 @@ entry_field.place(x=20 , y=100)
 def Text_to_speech():
     Message = entry_field.get()
     speech = gTTS(text = Message)
-    speech.save('DataFlair.mp3')
-    playsound('DataFlair.mp3')
+    speech.save('aloha-tts-convertor.mp3')
+    playsound('aloha-tts-convertor.mp3')
 
 def Exit():
     root.destroy()
