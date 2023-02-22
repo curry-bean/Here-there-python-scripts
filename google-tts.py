e8a2c5daf94eb32c0f373c0e6e793360dbef48a3
curry-bean <curry.bean.017@gmail.com>
2023-02-22 23:01:03 +0300

    5th Commit

---
+++ google-tts.py
@@ -0,0 +1,36 @@
+from google.cloud import texttospeech
+import os
+from playsound import playsound
+
+def text_to_speech(text):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+
+    # Build the voice request, select the language code ("en-US") and the ssml voice gender
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+
+    # Play the output.mp3 file
+    os.system("afplay output.mp3")
+
+    text_to_speech("Hello, world!")
+
+    playsound('aloha-tts-convertor.mp3')
